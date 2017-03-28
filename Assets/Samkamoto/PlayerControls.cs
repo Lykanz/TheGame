df769533980802ebbe1d52fc7bf9059faddb122d
Sameow <samuelaxl@gmail.com>
2017-03-28 18:40:55 +0800

    asdasda
    sdasdasd

---
+++ Assets/Samkamoto/PlayerControls.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+    public float speed;
+    public float jumpheight;
+    bool isGrounded;
+    Vector3 jump;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-speed, 0.0f);
+
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(speed, 0.0f);
+
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(jump * jumpheight, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+
+    }
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+}
