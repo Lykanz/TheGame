b391075c4d03a5caa560008ae5c8a2ed15c89fd2
Sameow <samuelaxl@gmail.com>
2017-03-28 19:39:13 +0800

    asd
    s

--- Assets/Samkamoto/PlayerControls.cs
+++ Assets/Samkamoto/PlayerControls.cs
@@ -6,6 +6,9 @@ public class PlayerControls : MonoBehaviour {
     public float speed;
     public float jumpheight;
     bool isGrounded;
+    float dashperiod = 1f;
+    bool isDashing = false;
+    float dashtime;
     Vector3 jump;
     Rigidbody rb;
 
@@ -17,28 +20,44 @@ public class PlayerControls : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.position += new Vector3(-speed, 0.0f);
-
-        }
-
+        bool isDashing = false;
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += new Vector3(speed, 0.0f);
-
         }
 
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-speed, 0.0f);
+        }
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            rb.AddForce(jump * jumpheight, ForceMode.Impulse);
             isGrounded = false;
+            rb.AddForce(jump * jumpheight, ForceMode.Impulse);
         }
 
 
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            isDashing = true;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && isDashing)
+        {
+            transform.position += new Vector3(1f, 0f);
+        }
+        if (isDashing)
+        {
+            dashtime += Time.deltaTime;
+            if (dashtime > dashperiod) {
+                isDashing = false;
+            }
+        }
+
+
+
     }
-    void OnCollisionStay()
+    void OnCollisionEnter(Collision collider)
     {
         isGrounded = true;
     }
