eef8e99b9eeb357df4584182fc1f0a67cb29a780
Lykanz <archjw@hotmail.com>
2017-03-28 18:38:29 +0800

    Firsto
    Added
    - Room Generator script (WIP lel)

---
+++ Assets/Lihkanz/RoomGenerator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomGenerator : MonoBehaviour {
+
+    public int totalAmountOfRooms, currentAmtRooms;
+    public enum RoomType{ monster, puzzle, trap, special }
+    RoomType currentRoomType;
+
+	// Use this for initialization
+	void Start () {
+        GenerateDungeon();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void GenerateDungeon()
+    {
+        while(currentAmtRooms < totalAmountOfRooms)
+        {
+            currentRoomType = (RoomType)Random.Range(0,4);
+            GenerateRoom(currentRoomType);
+            currentAmtRooms++;
+        }
+    }
+
+    void GenerateRoom(RoomType _currentRoomType)
+    {
+        Debug.Log(_currentRoomType);
+    }
+}
